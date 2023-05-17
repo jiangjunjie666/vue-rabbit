6894385617ecbabc57f593ba3a58ea161b459409
jiangjunjie666 <2631261619@qq.com>
2023-05-17 17:16:49 +0800

    element主题色定制的配置

--- vite.config.js
+++ vite.config.js
@@ -14,12 +14,25 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()]
     }),
     Components({
-      resolvers: [ElementPlusResolver()]
+      //1.配置elementPLus才哟个sass样式的配色系统
+      // resolvers: [ElementPlusResolver()]
+      resolvers: [ElementPlusResolver({ importStyle: 'sass' })]
     })
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  //2.自动导入定制化样式文件进行样式覆盖
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `
+      }
     }
   }
 })
