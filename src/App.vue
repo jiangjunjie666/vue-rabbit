40d7140908a7eae219787c25a757e01f928480c2
jiangjunjie666 <2631261619@qq.com>
2023-05-17 16:14:24 +0800

    完成别名联想设置

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue'
 </script>
 
 <template>
